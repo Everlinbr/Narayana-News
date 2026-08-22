9759a6836b5ad1f2492727a4450c44efc2cf1215
Everlinbr5106A <elvinbr5106a@gmail>
2026-08-22 20:17:18 +0530

    Improved the Animation

--- Pymations/logo-1080p-svg.py
+++ Pymations/logo-1080p-svg.py
@@ -2,8 +2,21 @@ from manim import *
 
 class logo(Scene):
     def construct(self):
-        logo=SVGMobject("../Resources/Images/logo.svg")
-        self.play(Write(logo))
+        logo = SVGMobject("../Resources/Images/logo.svg")
+        text = Text("Narayana Public School", font_size=44)
+        text.next_to(logo, DOWN)
+        self.play(
+            AnimationGroup(
+                Write(logo),
+                Write(text),
+                lag_ratio=0
+                        )
+            )
         self.wait(4)
+
+
+        #self.play(Write(text))
+        #self.play(Write(logo))
+        #self.wait(4)
 
 
