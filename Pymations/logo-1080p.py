8d246d9c4448f73c07e665d1a35a409e01df981a
Everlinbr5106A <elvinbr5106a@gmail.com>
2026-08-25 06:32:09 +0530

    Deleted Pymations/logo-1080p.py

--- Pymations/logo-1080p.py
+++
@@ -1,9 +0,0 @@
-from manim import *
-
-class logo(Scene):
-    def construct(self):
-        logo=ImageMobject("../Resources/Images/logo-nps.png")
-        self.play(FadeIn(logo))
-        self.wait(4)
-
-
